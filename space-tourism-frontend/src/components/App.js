6d74a55a6021941a96815fe26ceb9645e78d63f6
Brent Besselievre <brent@Brents-MacBook-Air.local>
2021-10-26 20:05:44 -0600

    Built basic Header component and added it to App.js

--- space-tourism-frontend/src/components/App.js
+++ space-tourism-frontend/src/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import Header from './Header';
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hello from React</h1>
     </div>
   );
