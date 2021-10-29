599ba718b1e72f827118eae3857a4b1d6f17593a
Brent Besselievre <brent@Brents-MacBook-Air.local>
2021-10-29 16:55:14 -0600

    Added content and inital styling to Home component

--- space-tourism-frontend/src/components/Home.js
+++ space-tourism-frontend/src/components/Home.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is the Home component</h1>
+        <div className='container'>
+
+            <h1 className='fs-500 txt-light uppercase ff-sans-cond letter-spacing-1'>So, you want to travel to <span className='fs-900 txt-white ff-serif'>Space</span></h1>
+
+            <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
+            outer space and not hover kind of on the edge of it. Well sit back, and relax 
+            because we’ll give you a truly out of this world experience!</p>
+            
         </div>
     );
 }
