9b396e5ead0449448ad514a6c89fafa12bb738d6
Brent Besselievre <brent@Brents-MacBook-Air.local>
2021-10-29 16:40:33 -0600

    More styling for header. Added an img. Doesn't show due to a bug with flex. I'll figure that out later

--- space-tourism-frontend/src/components/Header.js
+++ space-tourism-frontend/src/components/Header.js
@@ -3,20 +3,23 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header className="primary-header">
+        <header className="primary-header flex">
+            <div>
+                <img src='./../../assets/shared/logo.svg' alt='space tourism logo' />
+            </div>
             <nav>
                 <ul className="primary-navigation underline-indicators flex">
                     <li>
-                        <Link to="/" className="txt-white uppercase letter-spacing-2"><span>00</span>Home</Link>
+                        <Link to="/" className="txt-white uppercase letter-spacing-2 ff-sans-cond"><span>00</span>Home</Link>
                     </li>
                     <li>
-                        <Link to="/" className="txt-white uppercase letter-spacing-2"><span>01</span>Destination</Link>
+                        <Link to="/" className="txt-white uppercase letter-spacing-2 ff-sans-cond"><span>01</span>Destination</Link>
                     </li>
                     <li>
-                        <Link to="/" className="txt-white uppercase letter-spacing-2"><span>02</span>Crew</Link>
+                        <Link to="/" className="txt-white uppercase letter-spacing-2 ff-sans-cond"><span>02</span>Crew</Link>
                     </li>
                     <li>
-                        <Link to="/" className="txt-white uppercase letter-spacing-2"><span>03</span>Technology</Link>
+                        <Link to="/" className="txt-white uppercase letter-spacing-2 ff-sans-cond"><span>03</span>Technology</Link>
                     </li>
                 </ul>
             </nav>
