2b50ded339fa0342ac8094f7a5cdd84c1a17d9ef
Brent Besselievre <brent@Brents-MacBook-Air.local>
2021-10-29 17:32:40 -0600

    Initial layout design of Home component

--- space-tourism-frontend/src/components/Home.js
+++ space-tourism-frontend/src/components/Home.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div className='container'>
+        <div className='container grid-container'>
+            <div>
+                <h1 className='fs-500 txt-light uppercase ff-sans-cond letter-spacing-1'>So, you want to travel to <span className='fs-900 txt-white ff-serif'>Space</span></h1>
 
-            <h1 className='fs-500 txt-light uppercase ff-sans-cond letter-spacing-1'>So, you want to travel to <span className='fs-900 txt-white ff-serif'>Space</span></h1>
-
-            <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
-            outer space and not hover kind of on the edge of it. Well sit back, and relax 
-            because we’ll give you a truly out of this world experience!</p>
-            
+                <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
+                outer space and not hover kind of on the edge of it. Well sit back, and relax 
+                because we’ll give you a truly out of this world experience!</p>
+            </div>
+            <div>
+                <Link to='/' className='large-button uppercase ff-serif fs-600 text-dark bg-white'>Explore</Link>
+            </div>
         </div>
     );
 }
