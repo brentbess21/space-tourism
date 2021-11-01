3bd1c5947e33da127a4daa773e86b49f1729e35b
Brent Besselievre <brent@Brents-MacBook-Air.local>
2021-11-01 10:06:59 -0600

    Minor layout changes

--- space-tourism-frontend/src/components/Home.js
+++ space-tourism-frontend/src/components/Home.js
@@ -3,16 +3,18 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div className= 'grid-container grid-container--home home'>
-            <div>
-                <h1 className='fs-500 txt-light uppercase ff-sans-cond letter-spacing-1'>So, you want to travel to <span className='fs-900 txt-white ff-serif display-block'>Space</span></h1>
+        <div className='home component-body'>
+            <div className= 'grid-container grid-container--home'>
+                <div>
+                    <h1 className='fs-500 txt-light uppercase ff-sans-cond letter-spacing-1'>So, you want to travel to <span className='fs-900 txt-white ff-serif display-block'>Space</span></h1>
 
-                <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
-                outer space and not hover kind of on the edge of it. Well sit back, and relax 
-                because we’ll give you a truly out of this world experience!</p>
-            </div>
-            <div>
-                <Link to='/' className='large-button uppercase ff-serif text-dark bg-white'>Explore</Link>
+                    <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
+                    outer space and not hover kind of on the edge of it. Well sit back, and relax 
+                    because we’ll give you a truly out of this world experience!</p>
+                </div>
+                <div>
+                    <Link to='/' className='large-button uppercase ff-serif text-dark bg-white'>Explore</Link>
+                </div>
             </div>
         </div>
     );
