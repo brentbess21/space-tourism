f881b8a0bbaf119725bb78d786485c9380cac10f
Brent Besselievre <brent@Brents-MacBook-Air.local>
2021-11-03 16:29:45 -0600

    Added content to Moon component

--- space-tourism-frontend/src/components/destinations/Destinations.js
+++ space-tourism-frontend/src/components/destinations/Destinations.js
@@ -10,11 +10,11 @@ const Destinations = () => {
     return(
         <div className='destinations component-body'>
              <Header />
-            <main className= 'grid-container grid-container--destination'>
+            <div className= 'grid-container grid-container--destination'>
                 <h1 className='numbered-title'><span className=''>01</span>Pick your destination</h1>
-                <img src='./../../assets/destination/image-moon.webp' alt='the moon'/>
                 <SubHeader />
-            </main>
+                <Moon />
+            </div>
         </div>
     )
 }
--- space-tourism-frontend/src/components/destinations/Moon.js
+++ space-tourism-frontend/src/components/destinations/Moon.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 const Moon = () => {
     return (
-        <div>
+        <article>
+            <img src='./../../assets/destination/image-moon.webp' alt='the moon' />
+            <h2>Moon</h2>
 
-        </div>
+            <p>See our planet as you’ve never seen it before. A perfect relaxing trip away to help 
+            regain perspective and come back refreshed. While you’re there, take in some history 
+            by visiting the Luna 2 and Apollo 11 landing sites.</p>
+
+            <h3>Avg. distance</h3>
+            <p>384,400 km</p>
+
+            <h3>Est. travel time</h3>
+            <p>3 days</p>
+        </article>
     )
 }
 
