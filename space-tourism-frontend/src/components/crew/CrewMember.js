ad6e85528fdff5f7d8b583557a4b28c62ff14138
Brent Besselievre <brent@Brents-MacBook-Air.local>
2021-11-07 11:39:35 -0700

    Dot indicator fixes for CrewMember component

--- space-tourism-frontend/src/components/crew/CrewMember.js
+++ space-tourism-frontend/src/components/crew/CrewMember.js
@@ -9,25 +9,17 @@ const CrewMember = (props) => {
     const { crewRole } = useParams();
 
     const crewMember = crew.find(member => member.role.replace(/\s+/g, '') === crewRole)
-    console.log("Test:", crewMember.role.replace(/\s+/g, ''));
  
     return (
         <div className='crew'>
             <Header />
             <main id="main" className="grid-container grid-container--crew flow">
                 <h1 className="numbered-title"><span aria-hidden="true">02</span> Meet the crew</h1>
-                {/* <div className="dot-indicators flex">
-                    <button><span className="sr-only">The commander</span></button>
-                    <button><span className="sr-only">The mission specialist</span></button>
-                    <button><span className="sr-only">The pilot</span></button>
-                    <button><span className="sr-only">The crew engineer</span></button>
-                </div> */}
-
-                <div className="tab-list underline-indicators flex">
-                    <Link to={`/crew/Commander`}  className="uppercase ff-sans-cond txt-light letter-spacing-2 no-decoration" >The Commander</Link>
-                    <Link to={`/crew/MissionSpecialist`}  className="uppercase ff-sans-cond txt-light letter-spacing-2 no-decoration" >Mission Specialist</Link>
-                    <Link to={`/crew/Pilot`}  className="uppercase ff-sans-cond txt-light letter-spacing-2 no-decoration" >The Pilot</Link>
-                    <Link to={`/crew/FlightEngineer`}  className="uppercase ff-sans-cond txt-light letter-spacing-2 no-decoration" >Flight Engineer</Link>
+                <div className="dot-indicators flex">
+                    <Link to={`/crew/Commander`}><span className="sr-only" >The commander</span></Link>
+                    <Link to={`/crew/MissionSpecialist`}><span className="sr-only" >Mission Specialist</span></Link>
+                    <Link to={`/crew/Pilot`}><span className="sr-only" >The Pilot</span></Link>
+                    <Link to={`/crew/FlightEngineer`}><span className="sr-only" >Flight Engineer</span></Link>
                 </div>
 
                 <article className="crew-details">
