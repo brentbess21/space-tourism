dc0438ae674bc6d7a98cb41eebed437dd849fd1a
Brent Besselievre <brent@brents-air.lan>
2021-11-08 16:06:57 -0700

    fixed issue with text alignment on Home component at mobile size

--- space-tourism-frontend/src/components/Home.js
+++ space-tourism-frontend/src/components/Home.js
@@ -6,11 +6,11 @@ import Header from './Header';
 const Home = (props) => {
 
     return (
-        <div className='home component-body'>
+        <div className='home'>
             <Header /> 
             <main className= 'grid-container grid-container--home'>
                 <div>
-                    <h1 className='fs-500 txt-light uppercase ff-sans-cond letter-spacing-1'>So, you want to travel to <span className='fs-900 txt-white ff-serif display-block'>Space</span></h1>
+                    <h1 className='fs-500 txt-light uppercase ff-sans-cond letter-spacing-1 home-title'>So, you want to travel to <span className='fs-900 txt-white ff-serif display-block'>Space</span></h1>
 
                     <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
                     outer space and not hover kind of on the edge of it. Well sit back, and relax 
